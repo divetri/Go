89a4614334eecf23dbe31e23aeff98bf58f211e1
divetri <divetrirahmawati@gmail.com>
2023-12-27 22:17:51 +0700

    yes

--- main.go
+++ main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("ok")
+
+	names := []string{"divet", "bambang", "paijo"}
+	for index, value := range names {
+		fmt.Printf("absen %v namanya adalah %v \n", index+1, value)
+	}
 }
