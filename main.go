8f7e9eab654889ba49934a90a5c4e55599b42d05
divetri <divetrirahmawati@gmail.com>
2023-12-27 22:33:58 +0700

    _ underscore var

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("ok")
 
+	// names := []string{"divet", "bambang", "paijo"}
+	// for index, value := range names {
+	// 	fmt.Printf("absen %v namanya adalah %v \n", index+1, value)
+	// }
+
 	names := []string{"divet", "bambang", "paijo"}
-	for index, value := range names {
-		fmt.Printf("absen %v namanya adalah %v \n", index+1, value)
+	for _, value := range names {
+		fmt.Printf("namanya adalah %v \n", value)
 	}
+
 }
