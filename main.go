8e4e3788a48f3edcf35674827e60d6155a5d3087
divetri <divetrirahmawati@gmail.com>
2024-01-19 12:05:52 +0700

    _

--- main.go
+++ main.go
@@ -3,47 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var menu, n1, n2, value int32
-	fmt.Println("Calculator")
-
-	fmt.Println("Choose menu:")
-	fmt.Println("1. Sum (+)")
-	fmt.Println("2. Sub (-)")
-	fmt.Println("3. Ply (*)")
-	fmt.Println("4. Div (/)")
-
-	fmt.Println("Choose menu:")
-	fmt.Scanln(&menu)
-
-	fmt.Println("Insert first number:")
-	fmt.Scanln(&n1)
-
-	fmt.Println("Insert second number:")
-	fmt.Scanln(&n2)
-
-	switch menu {
-	case 1:
-		value = n1 + n2
-		fmt.Println(n1, "+", n2, "=", value)
-	case 2:
-		value = n1 - n2
-		fmt.Println(n1, "-", n2, "=", value)
-	case 3:
-		value = n1 * n2
-		fmt.Println(n1, "*", n2, "=", value)
-	case 4:
-		value = n1 / n2
-		fmt.Println(n1, "/", n2, "=", value)
-	}
+	fmt.Println("ok")
 
 	// names := []string{"divet", "bambang", "paijo"}
 	// for index, value := range names {
 	// 	fmt.Printf("absen %v namanya adalah %v \n", index+1, value)
 	// }
 
-	// names := []string{"divet", "bambang", "paijo"}
-	// for _, value := range names {
-	// 	fmt.Printf("namanya adalah %v \n", value)
-	// }
-
+	names := []string{"divet", "bambang", "paijo"}
+	for _, value := range names {
+		fmt.Printf("namanya adalah %v \n", value)
+	}
 }
---
+++ simple-calculator.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func calculator() {
+	var menu, n1, n2, value int32
+	fmt.Println("Calculator")
+
+	fmt.Println("Choose menu:")
+	fmt.Println("1. Sum (+)")
+	fmt.Println("2. Sub (-)")
+	fmt.Println("3. Ply (*)")
+	fmt.Println("4. Div (/)")
+
+	fmt.Println("Choose menu:")
+	fmt.Scanln(&menu)
+
+	fmt.Println("Insert first number:")
+	fmt.Scanln(&n1)
+
+	fmt.Println("Insert second number:")
+	fmt.Scanln(&n2)
+
+	switch menu {
+	case 1:
+		value = n1 + n2
+		fmt.Println(n1, "+", n2, "=", value)
+	case 2:
+		value = n1 - n2
+		fmt.Println(n1, "-", n2, "=", value)
+	case 3:
+		value = n1 * n2
+		fmt.Println(n1, "*", n2, "=", value)
+	case 4:
+		value = n1 / n2
+		fmt.Println(n1, "/", n2, "=", value)
+	}
+
+	// names := []string{"divet", "bambang", "paijo"}
+	// for index, value := range names {
+	// 	fmt.Printf("absen %v namanya adalah %v \n", index+1, value)
+	// }
+
+	// names := []string{"divet", "bambang", "paijo"}
+	// for _, value := range names {
+	// 	fmt.Printf("namanya adalah %v \n", value)
+	// }
+
+}
